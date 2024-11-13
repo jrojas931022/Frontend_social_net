507166f2a793590b65d2445a83687acc9414447b
jrojas931022 <jjru931022@gmail.com>
2024-11-13 12:03:44 -0500

    Add files via upload

---
+++ src/helpers/Global.jsx
@@ -0,0 +1,14 @@
+// Ruta del Backend desplegado
+  export const Global = {
+    url: "https://backend-social-net-7iu4.onrender.com/api/"
+  }
+
+
+
+
+// Ruta del Backend en Local: se debe ejecutar el servidor del backend
+/*
+export const Global = {
+    url: "http://localhost:3900/api/"
+  }
+*/
